72df002f396e51d546f90508cec7dd1377d5bca5
JamesYang <gn00999829@gmail.com>
2020-07-06 13:17:52 -0700

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'tui-image-editor/dist/tui-image-editor.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import Toast from './components/Toast';
@@ -11,6 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
+          <Redirect exact from="/" to="/dashboard" />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/toastui" component={Toast} />
         </Switch>
